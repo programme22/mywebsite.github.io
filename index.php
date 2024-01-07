6e945e5f921ae4908ae8393197f90903da14607e
programme22 <118743902+programme22@users.noreply.github.com>
2024-01-07 19:25:04 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>1st project</title>
+    </head>
+    <body>
+        <?php
+            echo "Hello";
+        ?>
+    </body>
+</html>
